710d1f754378ac9911682f246da89c9181b0ea51
godevil734 <godevil734@gmail.com>
2026-06-12 17:49:55 +0530

    .

--- week 2/Day 1/FirstUniqueCharacterInaString.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int firstUniqChar(string s) {
-        for (int i = 0; i < s.size(); i++)
-            if (s.find(s[i]) == s.rfind(s[i]))
-                return i;
-        return -1;
-    }
-};
